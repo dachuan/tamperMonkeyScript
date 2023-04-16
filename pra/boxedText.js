93d1b80cf104c9aa3d5ad196b6fab20a9548e97f
dcthe <tianiao@126.com>
2023-04-16 17:47:58 +0800

    文字框选 pra 以便扩展使用

---
+++ pra/boxedText.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         boxed text
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+ // ==UserScript== // @name Add Red Border to Selected Text // @namespace http://tampermonkey.net/ // @version 1.0 // @description Adds a red border to selected text on the page. // @match *://*/* // @grant none // ==/UserScript==
+(function() {
+    'use strict';
+    // Add event listener to detect when text is selected
+    document.addEventListener('mouseup', function() { var selection = window.getSelection(); if (selection.toString().length) {
+        // Wrap selected text in a span element with red border
+        var span = document.createElement('span');
+        span.style.border = '2px solid red';
+        selection.getRangeAt(0).surroundContents(span); } }); })();
