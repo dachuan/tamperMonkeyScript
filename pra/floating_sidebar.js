f4cf9ae60de6a3f53b7cfbbb5ddb9915183ce00d
dcthe <tianiao@126.com>
2023-04-20 11:37:51 +0800

    调整了siderbar中的title与copy all的样式

--- pra/floating_sidebar.js
+++ pra/floating_sidebar.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Floating sidebar
 // @namespace    http://tampermonkey.net/
-// @version      0.0.7
+// @version      0.0.9
 // @description  select text and send to sidebar 
 // @author       dcthehiker
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
 // @grant        none
 // ==/UserScript==
-
 
 (function() {
     'use strict';
@@ -20,13 +19,15 @@
     sidebar.style.right = '0';
     sidebar.style.transform = 'translateY(-50%)';
     sidebar.style.backgroundColor = '#ffffff';
-    sidebar.style.padding = '10px';
+    sidebar.style.padding = '0px 0px 0px 10px';
     sidebar.style.border = '1px solid #ccc';
     sidebar.style.boxShadow = '2px 2px 4px #ccc';
-    sidebar.style.maxWidth = '300px'; // Add maximum width to the sidebar
-    sidebar.style.maxHeight = '500px'; // Add maximum height to the sidebar
+    //sidebar.style.maxWidth = '300px'; // Add maximum width to the sidebar
+    sidebar.style.width = '400px'; // Set fixed width
+    sidebar.style.maxHeight = '300px'; // Add maximum height to the sidebar
     sidebar.style.overflowY = 'auto'; // Add scrollbar to the sidebar
-    sidebar.style.wordWrap = 'break-word'; // Allow text to wrap to next line   sidebar.style.zIndex = '9999';
+    sidebar.style.wordWrap = 'break-word'; // Allow text to wrap to next line
+    sidebar.style.zIndex = '9999';
     sidebar.style.fontFamily = 'Arial, sans-serif';
     sidebar.style.fontSize = '12px';
 
@@ -39,11 +40,34 @@
     title.style.color = '#fff';
     title.style.backgroundColor = '#42bbf4';
     title.style.fontWeight = 'bold';
-    sidebar.appendChild(title);
+
+    // Make the title fixed at the top of the sidebar
+    var titleContainer = document.createElement('div');
+    titleContainer.appendChild(title);
+    titleContainer.style.position = 'sticky';
+    titleContainer.style.top = '0';
+    sidebar.appendChild(titleContainer);
+
+    // Create a button to copy all sidebar items to clipboard
+    var copyAllButton = document.createElement('button');
+    copyAllButton.textContent = 'copy all';
+    copyAllButton.style.position = 'sticky';
+    copyAllButton.style.top = '24px';
+    //copyAllButton.style.bottom = '10px';
+    //copyAllButton.style.right = '10px';
+    copyAllButton.addEventListener('click', function(event) {
+        var sidebarText = '';
+        var sidebarItems = sidebar.querySelectorAll('li');
+        for (var i = 0; i < sidebarItems.length; i++) {
+            sidebarText += sidebarItems[i].textContent + '\n';
+        }
+        navigator.clipboard.writeText(sidebarText);
+    });
+    sidebar.appendChild(copyAllButton);
 
     document.body.appendChild(sidebar);
 
-   // Define Diigo style for list items
+    // Define Diigo style for list items
     var listItemStyle = 'list-style-type: none;' +
                          'padding: 10px 5px;' +
                          'margin: 10px 0;' +
@@ -53,7 +77,7 @@
                          'font-weight: normal;' +
                          'color: #333;';
 
-     // Keep track of selected text to avoid duplicates
+    // Keep track of selected text to avoid duplicates
     var selectedText = [];
 
     // Listen for selection events on the page
@@ -61,16 +85,31 @@
         var selection = window.getSelection().toString().trim();
         var target = event.target;
         if (selection.length > 0 && !selectedText.includes(selection) && !sidebar.contains(target)) {
+
             // Create a list item for the selected text with Diigo style
             var listItem = document.createElement('li');
             listItem.setAttribute('style', listItemStyle);
             listItem.textContent = selection;
 
+            // set opacity to 0
+            listItem.style.opacity = 0;
+
             // Add the list item to the sidebar
             sidebar.appendChild(listItem);
+
+            // Animate adding text to the sidebar
+            var opacity = 0;
+            var interval = setInterval(function(){
+                opacity += 0.1;
+                listItem.style.opacity = opacity;
+                if(opacity >= 1){
+                    clearInterval(interval);
+                }
+            }, 50);
 
             // Add the selected text to the array to avoid duplicates
             selectedText.push(selection);
         }
     });
+
 })();
