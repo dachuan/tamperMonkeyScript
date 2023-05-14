f28c8f17e49c55a640c16a3c3a11579afd77f3a8
dcthe <tianiao@126.com>
2023-05-14 14:35:29 +0800

    keylistener for meta key

--- pra/keyListener.js
+++ pra/keyListener.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KeyListener
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  key listener for activate some function
 // @author       dcthehiker
 // @match        *://*/*
@@ -9,59 +9,81 @@
 // @grant        none
 // ==/UserScript==
 
+/* add metakey */
 (function() {
     'use strict';
 
-// Define the key combination to listen for
-const keys = {
-  ctrl: false,
-  alt: false,
-  shift: false,
-  key: ''
-};
+    // Define the key combination to listen for
+    const keys = {
+        ctrl: false,
+        alt: false,
+        shift: false,
+        meta: false,
+        key: ''
+    };
 
-// Listen for keydown events
-document.addEventListener('keydown', event => {
-  // Update the keys object based on which key was pressed
-  switch (event.key) {
-    case 'Control':
-      keys.ctrl = true;
-      break;
-    case 'Alt':
-      keys.alt = true;
-      break;
-    case 'Shift':
-      keys.shift = true;
-      break;
-    default:
-      keys.key = event.key;
-      break;
-  }
+    // Listen for keydown events
+    document.addEventListener('keydown', event => {
+        switch (event.key) {
+          case 'Control':
+            keys.ctrl = true;
+            break;
+          case 'Alt':
+            keys.alt = true;
+            break;
+          case 'Shift':
+            keys.shift = true;
+            break;
+          case 'Meta':
+            keys.meta = true;
+            break;
+          default:
+            keys.key = event.key;
+            break;
+        }
+    
+      // Check if the key combination was pressed
+      if (keys.ctrl && keys.shift && keys.key === 'K') {
+        // Do something here
+        console.log("combo shift key pressed.");
+      }
 
-  // Check if the key combination was pressed
-  if (keys.ctrl && keys.shift && keys.key === 'K') {
-    // Do something here
-    console.log("combo key pressed.");
-  }
-});
+      // Check if the key combination was pressed
+      if (keys.ctrl && keys.alt && keys.key === 'K') {
+          // Do something here
+          console.log("combo alt key pressed.");
+      }
 
-// Listen for keyup events
-document.addEventListener('keyup', event => {
-  // Update the keys object based on which key was released
-  switch (event.key) {
-    case 'Control':
-      keys.ctrl = false;
-      break;
-    case 'Alt':
-      keys.alt = false;
-      break;
-    case 'Shift':
-      keys.shift = false;
-      break;
-    default:
-      keys.key = '';
-      break;
-  }
-});
+      // Check if the key combination was pressed
+      if (keys.shift && keys.meta && keys.key === 'k') {
+          // Do something here
+          console.log("combo meta key pressed.");
+      }
+
+
+      //console.log(keys);
+    });
+
+    // Listen for keyup events
+    document.addEventListener('keyup', event => {
+        // Update the keys object based on which key was released
+        switch (event.key) {
+          case 'Control':
+            keys.ctrl = false;
+            break;
+          case 'Alt':
+            keys.alt = false;
+            break;
+          case 'Shift':
+            keys.shift = false;
+            break;
+          case 'Meta':
+            keys.meta = false;
+            break;
+          default:
+            keys.key = '';
+            break;
+        }
+    });
 })();
 
