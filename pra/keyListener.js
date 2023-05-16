1da2c0619f03fbc87b2716e869e4213a3d4ffbe0
dcthe <tianiao@126.com>
2023-05-16 13:51:31 +0800

    使用GPT写的代码靠谱，并且高级得多。实现了单击，连击，组合键，leader键

--- pra/keyListener.js
+++ pra/keyListener.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KeyListener
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.3
 // @description  key listener for activate some function
 // @author       dcthehiker
 // @match        *://*/*
@@ -9,85 +9,138 @@
 // @grant        none
 // ==/UserScript==
 
-/* add metakey */
+/* add metakey 
+ * combination keys
+ * double keys
+ * leader key, combo
+ * by GPT-4
+ * */
 (function() {
     'use strict';
 
-    // Define the key combination to listen for
-    const keys = {
-        ctrl: false,
-        alt: false,
-        shift: false,
-        meta: false,
-        key: ''
-    };
-
-    // Listen for keydown events
-    document.addEventListener('keydown', event => {
-        switch (event.key) {
-          case 'Control':
-            keys.ctrl = true;
-            break;
-          case 'Alt':
-            keys.alt = true;
-            break;
-          case 'Shift':
-            keys.shift = true;
-            break;
-          case 'Meta':
-            keys.meta = true;
-            break;
-          default:
-            keys.key = event.key;
-            break;
+    class KeyHandler {
+      constructor() {
+        this.keyDown = {};
+        this.lastKey = null;
+        this.lastKeyDownTime = null;
+        this.leaderKeyDown = false;
+        this.leaderKey = [];
+    
+        document.addEventListener('keydown', (e) => this.handleKeyDown(e));
+        document.addEventListener('keyup', (e) => this.handleKeyUp(e));
+      }
+    
+      handleKeyDown(e) {
+        this.keyDown[e.key] = true;
+    
+        if (this.leaderKeyDown) {
+          this.checkLeaderFollowKey(e.key);
         }
-      //console.log(keys);
-    });
-
-    // Listen for keyup events
-    document.addEventListener('keyup', event => {
-        // Update the keys object based on which key was released
-        switch (event.key) {
-          case 'Control':
-            keys.ctrl = false;
-            break;
-          case 'Alt':
-            keys.alt = false;
-            break;
-          case 'Shift':
-            keys.shift = false;
-            break;
-          case 'Meta':
-            keys.meta = false;
-            break;
-          default:
-            keys.key = '';
-            break;
-        }
-    });
-
-    function listenAt(isSetKeys,callback){
-        // check global keys combination
-        // isSetKeys 是一个函数
-        // 用来判断按键组合
-        document.addEventListener('keydown', function(event) {
-            console.log('key pressed');
-            if (isSetKeys())
-            {
-                console.log('keys satisfied.');
-                callback();
-            }
+      }
+    
+      handleKeyUp(e) {
+        this.keyDown[e.key] = false;
+      }
+    
+      singleKey(key, callback) {
+        document.addEventListener('keydown', (e) => {
+          if (e.key === key) {
+            callback();
+            //console.log(this.keyDown);
+          }
         });
+      }
+    
+      combinationKey(keys, callback) {
+        document.addEventListener('keydown', (e) => {
+          const allKeysPressed = keys.every((key) => this.keyDown[key]);
+          if (allKeysPressed) {
+            callback();
+            //console.log(this.keyDown);
+          }
+        });
+      }
+    
+      doubleKey(key, interval, callback) {
+        document.addEventListener('keydown', (e) => {
+          if (e.key === key) {
+            const currentTime = new Date().getTime();
+            if (this.lastKey === key && currentTime - this.lastKeyDownTime < interval) {
+              callback();
+            }
+            this.lastKey = key;
+            this.lastKeyDownTime = currentTime;
+          }
+        });
+      }
+    
+      comboKey(leaderKeys, followKey, callback) {
+        this.leaderKey = leaderKeys;
+        document.addEventListener('keydown', (e) => {
+          const allKeysPressed = leaderKeys.every((key) => this.keyDown[key]);
+          if (allKeysPressed) {
+            this.leaderKeyDown = true;
+          }
+        });
+    
+        //document.addEventListener('keyup', (e) => {
+        //  const anyLeaderKeyReleased = leaderKeys.some((key) => e.key === key);
+        //  if (anyLeaderKeyReleased) {
+        //    //this.leaderKeyDown = false;
+        //  }
+        //});
+    
+        this.checkLeaderFollowKey = (key) => {
+          if (key === followKey) {
+            callback();
+          }
+          this.leaderKeyDown = false;
+        };
+      }
     }
 
-    function onAction(){
-        console.log('combo key pressed');
-        console.log(keys);
-    }
-
-    listenAt(() => {return keys.ctrl && keys.key == 'a'}, onAction);
-    listenAt(() => {return keys.shift && keys.key == 'K'}, onAction);
-    listenAt(() => {return keys.ctrl && keys.meta  && keys.key == 'k'}, onAction);
+    const keyHandler = new KeyHandler();
+    
+    keyHandler.singleKey('k', () => {
+      console.log('k');
+    });
+    
+    keyHandler.combinationKey(['Control', 'k'], () => {
+      console.log('Control, k');
+    });
+    
+    keyHandler.combinationKey(['Meta', 'k'], () => {
+      console.log('Meta, k');
+        // bug : k is not released
+    });
+    
+    keyHandler.combinationKey(['Meta', 'j'], () => {
+      console.log('Meta, j');
+    });
+    
+    keyHandler.combinationKey(['Control','Shift', 'K'], () => {
+      console.log('Control, shift, k');
+    });
+    
+    keyHandler.combinationKey(['Control', 'j'], () => {
+      console.log('Control, j');
+    });
+    
+    keyHandler.doubleKey('k', 1000, () => {
+      console.log('double k');
+    });
+    
+    keyHandler.doubleKey('j', 1000, () => {
+      console.log('double j');
+    });
+    
+    keyHandler.comboKey(['Control', 'q'], 'k', () => {
+      console.log('leader key is ctrl+q, follow key is k');
+    });
+    
+    keyHandler.comboKey(['Control', 'q'], 'j', () => {
+      console.log('leader key is ctrl+q, follow key is j');
+    });
 
 })();
 
