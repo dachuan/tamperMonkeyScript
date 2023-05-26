eaf6b8fc92aaa268ef1884559763e1fa65eceb46
dcthe <tianiao@126.com>
2023-05-26 21:27:58 +0800

    iframe中实现了右侧excalidraw画板

--- pra/excalidrawFloating.js
+++ pra/excalidrawFloating.js
@@ -12,17 +12,6 @@
     'use strict';
 
     // Get the HTML code to insert as an iframe
-    const _html = `<!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-            </head>
-            <body>
-                hello tm script!
-            </body>
-        </html>`;
 
     const html = `<!DOCTYPE html>
         <html>
